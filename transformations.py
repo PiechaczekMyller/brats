1be8deae788d67f361632056604bfc6b34056ad6
Michał Myller <michalmyll@gmail.com>
2019-12-09 21:58:36 +0100

    added Histogram Matching transformation

---
+++ transformations.py
@@ -0,0 +1,73 @@
+import numpy as np
+CHANNELS = 3
+
+
+class HistogramMatchingTransform:
+    """
+    Transformation performing histogram matching of volumes
+    """
+    def __init__(self, template: np.ndarray):
+        """
+
+        Args:
+            template (np.ndarray): Template which will be used for histogram
+                matching in future calls.
+                Dimensions - (Height, Width, Depth, Chennel)
+        """
+        self.template = template
+
+    def __call__(self, volume: np.ndarray) -> np.ndarray:
+        """
+        Args:
+            volume (np.ndarray): Volume to be matched.
+                Dimensions - (Height, Width, Depth, Channel)
+
+        Returns:
+             np.ndarray: Normalized volume with the same shape as input volume
+        """
+        result = np.zeros_like(volume)
+        for channel in range(volume.shape[CHANNELS]):
+            result[..., channel] = self.hist_match(volume[..., channel],
+                                                   self.template[..., channel])
+        return result
+
+    @staticmethod
+    def hist_match(source: np.ndarray, template: np.ndarray) -> np.ndarray:
+        """
+            Adjust the pixel values of a grayscale image such that its histogram
+            matches that of a target image
+
+            Args:
+                source (np.ndarray): Image to transform; the histogram is
+                    computed over the flattened array
+                template (np.ndarray): Template image; can have different
+                    dimensions to source
+            Returns:
+                np.ndarray: The transformed output image
+            """
+        nonzero = source > 0
+        result = np.zeros_like(source)
+
+        source = source[source > 0].ravel()
+        template = template[template > 0].ravel()
+
+        # get the set of unique pixel values and their corresponding indices and
+        # counts
+        s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                                return_counts=True)
+        t_values, t_counts = np.unique(template, return_counts=True)
+
+        # take the cumsum of the counts and normalize by the number of pixels to
+        # get the empirical cumulative distribution functions for the source and
+        # template images (maps pixel value --> quantile)
+        s_quantiles = np.cumsum(s_counts).astype(np.float64)
+        s_quantiles /= s_quantiles[-1]
+        t_quantiles = np.cumsum(t_counts).astype(np.float64)
+        t_quantiles /= t_quantiles[-1]
+
+        # interpolate linearly to find the pixel values in the template image
+        # that correspond most closely to the quantiles in the source image
+        interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+        result[nonzero] = interp_t_values[bin_idx]
+
+        return result  # interp_t_values[bin_idx].reshape(oldshape)
