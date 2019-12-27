8397e0ea38e14a728bd106d2ea512fe5cb9a046b
Szymon Piechaczek <szymon.piechaczek@yahoo.com>
2019-12-27 15:19:51 +0100

    Added combined dataset

--- brats/data/datasets.py
+++ brats/data/datasets.py
@@ -1,4 +1,5 @@
 import os
+import typing
 
 import nibabel as nib
 from torch.utils import data
@@ -21,14 +22,30 @@ class NiftiFolder(data.Dataset):
         self._files = list(os.scandir(root))
         self._transform = transform
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._files)
 
-    def __getitem__(self, item):
-        scan = nib.load(self._files[item].path)
+    def __getitem__(self, idx: int) -> typing.Any:
+        scan = nib.load(self._files[idx].path)
         scan_array = scan.get_fdata()
 
         if self._transform:
             scan_array = self._transform(scan_array)
 
         return scan_array
+
+
+class CombinedDataset(data.Dataset):
+    """
+    Takes two datasets of the same length and combines them,
+    """
+
+    def __init__(self, *datasets: data.Dataset):
+        assert all(len(dataset) == len(datasets[0]) for dataset in datasets), "Length of both datasets must match"
+        self.datasets = datasets
+
+    def __len__(self):
+        return len(self.datasets[0])
+
+    def __getitem__(self, idx: int) -> typing.Tuple[typing.Any, ...]:
+        return tuple(dataset[idx] for dataset in self.datasets)
--- tests/data/test_datasets.py
+++ tests/data/test_datasets.py
@@ -15,7 +15,7 @@ IMAGES = 2
 def input_directory(tmpdir_factory):
     temp_dir = tmpdir_factory.mktemp("input_images")
     for i in range(IMAGES):
-        image = np.zeros(INPUT_IMAGE_SHAPE, dtype=np.uint8)
+        image = np.zeros(INPUT_IMAGE_SHAPE, dtype=np.uint8) + i
         nib_img = nib.Nifti1Image(image, affine=np.eye(4))
         nib.save(nib_img, os.path.join(temp_dir, f"image{i}.nii.gz"))
     return temp_dir
@@ -38,3 +38,31 @@ class TestNiftiFolder:
 
         for idx in range(len(dataset)):
             assert np.all(dataset[idx].shape == np.array(INPUT_IMAGE_SHAPE) + 4)
+
+
+class TestCombinedDataset:
+    def test_returns_correct_length(self, input_directory):
+        dataset1 = datasets.NiftiFolder(input_directory)
+        dataset2 = datasets.NiftiFolder(input_directory)
+        dataset3 = datasets.NiftiFolder(input_directory)
+
+        dataset = datasets.CombinedDataset(dataset1, dataset2, dataset3)
+        assert len(dataset) == IMAGES
+
+    def test_returns_correct_entries(self, input_directory):
+        dataset1 = datasets.NiftiFolder(input_directory)
+        dataset2 = datasets.NiftiFolder(input_directory)
+        dataset3 = datasets.NiftiFolder(input_directory)
+
+        dataset = datasets.CombinedDataset(dataset1, dataset2, dataset3)
+
+        assert all(np.array_equal(entry, dataset1[0]) for entry in dataset[0])
+
+    def test_if_raises_on_unequal_length(self, input_directory):
+        dataset1 = datasets.NiftiFolder(input_directory)
+        dataset2 = datasets.NiftiFolder(input_directory)
+        dataset3 = datasets.NiftiFolder(input_directory)
+
+        dataset2._files = [dataset2._files[0]]
+        with pytest.raises(AssertionError):
+            datasets.CombinedDataset(dataset1, dataset2, dataset3)
