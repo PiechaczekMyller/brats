1ce31433fa2bc596cbb5be601730898e7b056ab3
Michał Myller <michalmyll@gmail.com>
2019-12-29 15:46:26 +0100

    added hausdorff distance and tests

--- brats/metrics.py
+++ brats/metrics.py
@@ -1,9 +1,14 @@
+import typing
+
 import torch
+import numpy as np
+import medpy.metric.binary as mp
 
 import brats.utils as utils
 
 CHANNELS_DIM = 1
 ONE_CLASS = 1
+FIRST_CLASS = 0
 
 
 def dice_score_one_class(prediction: torch.Tensor, target: torch.Tensor,
@@ -71,7 +76,8 @@ def precision_score(prediction: torch.Tensor, target: torch.Tensor,
     all_but_batch_dims = list(range(1, target.dim()))
     true_positives = utils.calculate_intersection(prediction, target,
                                                   dim=all_but_batch_dims)
-    false_positive = utils.calculate_false_positives(prediction, target)
+    false_positive = utils.calculate_false_positives(prediction, target,
+                                                     dim=all_but_batch_dims)
     return true_positives / (true_positives + false_positive + epsilon)
 
 
@@ -92,4 +98,56 @@ def f_score(prediction: torch.Tensor, target: torch.Tensor, beta: float,
     precision = precision_score(prediction, target)
     recall = recall_score(prediction, target)
     return (1 + beta ** 2) * ((precision * recall) / (
-                beta ** 2 * precision + recall + epsilon))
+            beta ** 2 * precision + recall + epsilon))
+
+
+def hausdorff_distance95(prediction: torch.Tensor,
+                         target: torch.Tensor) -> torch.Tensor:
+    """
+    95th percentile of the Hausdorff Distance.
+
+    Computes the 95th percentile of the (symmetric) Hausdorff Distance (HD)
+    between the binary objects in two images.
+    Heavily depends on medpy.metric.binary.hd95, so check it out for more
+    details.
+    Args:
+        prediction: Network output.
+            Dimensions - (Batch, Class, Depth, Height, Width)
+        target: Target values.
+            Dimensions - (Batch, Class, Depth, Height, Width)
+    Returns:
+        torch.Tensor: Hausdorff distance for each slice.
+                Dimensions - (Batch, Depth)
+    """
+    assert utils.is_binary(prediction), "Predictions must be binary"
+    assert utils.is_binary(target), "Target must be binary"
+    prediction = prediction.cpu().detach().numpy()
+    target = target.cpu().detach().numpy()
+    volumes_count, _, slices_count, _, _ = prediction.shape
+    results = np.zeros((volumes_count, slices_count))
+    for volume_idx in range(volumes_count):
+        for slice_idx in range(slices_count):
+            prediction_slice = prediction[
+                volume_idx, FIRST_CLASS, slice_idx, ...]
+            target_slice = target[volume_idx, FIRST_CLASS, slice_idx, ...]
+            results[volume_idx, slice_idx] = mp.hd95(prediction_slice,
+                                                     target_slice)
+    return torch.from_numpy(results)
+
+
+def hausdorff_distance95_mean_max(prediction: torch.Tensor,
+                                  target: torch.Tensor) -> \
+                                  typing.Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Merges the hausdorff distances for each volume, using mean and max.
+    Args:
+        prediction: Network output.
+            Dimensions - (Batch, Class, Depth, Height, Width)
+        target: Target values.
+            Dimensions - (Batch, Class, Depth, Height, Width)
+    Returns:
+        tuple: Tuple with max and average for each volume.
+    """
+    results_per_slice = hausdorff_distance95(prediction, target)
+    return torch.mean(results_per_slice, dim=CHANNELS_DIM), \
+           torch.max(results_per_slice, dim=CHANNELS_DIM)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,7 +2,7 @@ import torch
 import pytest
 import numpy as np
 
-from brats.metrics import dice_score_one_class
+import brats.metrics as metrics
 
 BATCH_DIMS = (2, 1, 3, 3, 3)
 CHANNEL_DIM = 1
@@ -12,13 +12,13 @@ class TestDiceScoreOneClass:
     def test_if_returns_1_for_perfect_fit(self):
         images = torch.ones(*BATCH_DIMS)
         target = torch.ones(*BATCH_DIMS)
-        assert all(np.isclose(dice_score_one_class(images, target), 1,
+        assert all(np.isclose(metrics.dice_score_one_class(images, target), 1,
                               atol=1.e-4))
 
     def test_if_returns_0_for_worst_fit(self):
         images = torch.zeros(*BATCH_DIMS)
         target = torch.ones(*BATCH_DIMS)
-        assert all(np.isclose(dice_score_one_class(images, target), 0,
+        assert all(np.isclose(metrics.dice_score_one_class(images, target), 0,
                               atol=1.e-4))
 
     @pytest.mark.parametrize("images, target, result",
@@ -34,7 +34,142 @@ class TestDiceScoreOneClass:
                               ])
     def test_if_returns_expected_values(self, images, target, result):
         assert all(np.isclose(
-            dice_score_one_class(images.unsqueeze(dim=CHANNEL_DIM),
+            metrics.dice_score_one_class(images.unsqueeze(dim=CHANNEL_DIM),
+                                         target.unsqueeze(dim=CHANNEL_DIM)),
+            result,
+            atol=1.e-3))
+
+
+class TestRecallScore:
+    def test_if_returns_1_for_perfect_fit(self):
+        images = torch.ones(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.recall_score(images, target), 1,
+                              atol=1.e-4))
+
+    def test_if_returns_0_for_worst_fit(self):
+        images = torch.zeros(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.recall_score(images, target), 0,
+                              atol=1.e-4))
+
+    @pytest.mark.parametrize("images, target, result",
+                             [(torch.tensor([[0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.334),
+                              (torch.tensor([[1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.6667),
+                              (torch.tensor([[0, 1, 1, 0], [0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.334),
+                              (torch.tensor([[1, 1, 1, 0], [1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.6667)
+                              ])
+    def test_if_returns_expected_values(self, images, target, result):
+        assert all(np.isclose(
+            metrics.recall_score(images.unsqueeze(dim=CHANNEL_DIM),
                                  target.unsqueeze(dim=CHANNEL_DIM)),
             result,
             atol=1.e-3))
+
+
+class TestPrecisionScore:
+    def test_if_returns_1_for_perfect_fit(self):
+        images = torch.ones(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.precision_score(images, target), 1,
+                              atol=1.e-4))
+
+    def test_if_returns_0_for_worst_fit(self):
+        images = torch.zeros(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.precision_score(images, target), 0,
+                              atol=1.e-4))
+
+    @pytest.mark.parametrize("images, target, result",
+                             [(torch.tensor([[0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.5),
+                              (torch.tensor([[1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.6667),
+                              (torch.tensor([[0, 1, 1, 0], [0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]), 0.5),
+                              (torch.tensor([[1, 1, 1, 0], [1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.6667)
+                              ])
+    def test_if_returns_expected_values(self, images, target, result):
+        assert all(np.isclose(
+            metrics.precision_score(images.unsqueeze(dim=CHANNEL_DIM),
+                                    target.unsqueeze(dim=CHANNEL_DIM)),
+            result,
+            atol=1.e-3))
+
+
+class TestFScore:
+    def test_if_returns_1_for_perfect_fit(self):
+        images = torch.ones(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.f_score(images, target, beta=1), 1,
+                              atol=1.e-4))
+
+    def test_if_returns_0_for_worst_fit(self):
+        images = torch.zeros(*BATCH_DIMS)
+        target = torch.ones(*BATCH_DIMS)
+        assert all(np.isclose(metrics.f_score(images, target, beta=1), 0,
+                              atol=1.e-4))
+
+    @pytest.mark.parametrize("images, target, result",
+                             [(torch.tensor([[0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.4),
+                              (torch.tensor([[1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.6667),
+                              (torch.tensor([[0, 1, 1, 0], [0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]), 0.4),
+                              (torch.tensor([[1, 1, 1, 0], [1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.6667)
+                              ])
+    def test_if_f1_returns_expected_values(self, images, target, result):
+        assert all(np.isclose(
+            metrics.f_score(images.unsqueeze(dim=CHANNEL_DIM),
+                            target.unsqueeze(dim=CHANNEL_DIM), beta=1),
+            result,
+            atol=1.e-3))
+
+    @pytest.mark.parametrize("images, target, result",
+                             [(torch.tensor([[0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.3577),
+                              (torch.tensor([[1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1]]), 0.6667),
+                              (torch.tensor([[0, 1, 1, 0], [0, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.3577),
+                              (torch.tensor([[1, 1, 1, 0], [1, 1, 1, 0]]),
+                               torch.tensor([[1, 1, 0, 1], [1, 1, 0, 1]]),
+                               0.6667)
+                              ])
+    def test_if_f2_returns_expected_values(self, images, target, result):
+        assert all(np.isclose(
+            metrics.f_score(images.unsqueeze(dim=CHANNEL_DIM),
+                            target.unsqueeze(dim=CHANNEL_DIM), beta=2),
+            result,
+            atol=1.e-3))
+
+
+class TestHausdorffDistance95:
+    @pytest.mark.parametrize("batch_size, result", [((2, 1, 3, 3, 3), (2, 3)),
+                                                    ((5, 1, 2, 2, 2), (5, 2)),
+                                                    ((1, 1, 1, 1, 1), (1, 1))])
+    def test_if_returns_correct_shapes(self, batch_size, result):
+        images = torch.ones(*batch_size)
+        target = torch.ones(*batch_size)
+        assert metrics.hausdorff_distance95(images, target).shape == result
+
+    @pytest.mark.parametrize("batch_size, result", [((2, 1, 3, 3, 3), (2, 3)),
+                                                    ((5, 1, 2, 2, 2), (5, 2)),
+                                                    ((1, 1, 1, 1, 1), (1, 1))])
+    def test_if_mean_max_returns_correct_shapes(self, batch_size, result):
+        images = torch.ones(*batch_size)
+        target = torch.ones(*batch_size)
+        assert len(metrics.hausdorff_distance95_mean_max(images, target)[0]) == \
+               result[0]
