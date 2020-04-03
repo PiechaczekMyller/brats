Fix metric calculation in runners

brats/training/runners.py
@@ -75,5 +75,5 @@ def run_validation_epoch(model: nn.Module, data_loader: data.DataLoader, criteri
             for metric_name in metrics:
                 metric_values = [value.item() for value in metrics[metric_name](output, target)]
                 out_metrics[metric_name].append(metric_values)
-        out_metrics = {metric_name: np.array(out_metrics[metric_name]).mean(axis=1) for metric_name in metrics.keys()}
+    out_metrics = {metric_name: np.array(out_metrics[metric_name]).mean(axis=0) for metric_name in metrics.keys()}
     return np.mean(losses), out_metrics